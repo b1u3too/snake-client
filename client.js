4cb9fbeb89628e8e60c6bb4e74b9a6ca5b745d3b
b1u3too <almadsen792@live.ca>
2022-02-23 04:47:02 +0000

    Implemented connect handler - message for user and name initial display

--- client.js
+++ client.js
@@ -14,6 +14,11 @@ const connect = function () {
     console.log(data);
   })
 
+  conn.on("connect", () => {
+    console.log("You've successfully connected to the game!");
+    conn.write("Name: ALM");
+  })
+
   return conn;
 };
 
